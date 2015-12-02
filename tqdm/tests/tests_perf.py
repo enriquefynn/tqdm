f488540236adf21a4d0642ff156d9b9be183f4dd
Stephen L <lrq3000@gmail.com>
2015-12-02 20:10:38 +0100

    better perf tests (mock StringIO object) + add hard perf tests
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- tqdm/tests/tests_perf.py
+++ tqdm/tests/tests_perf.py
@@ -41,25 +41,32 @@ def relative_timer():
     elapser = lambda: spent
 
 
+class MockFileNoWrite(StringIO):
+    """ Wraps StringIO to mock a file with no I/O """
+    def write(self, data):
+        return
+
+
 def test_iter_overhead():
     """ Test overhead of iteration based tqdm """
     total = int(1e6)
 
-    with closing(StringIO()) as our_file:
+    with closing(MockFileNoWrite()) as our_file:
         a = 0
         with relative_timer() as time_tqdm:
             for i in trange(total, file=our_file):
                 a += i
         assert(a == (total * total - total) / 2.0)
 
-    a = 0
-    with relative_timer() as time_bench:
-        for i in _range(total):
-            a += i
+        a = 0
+        with relative_timer() as time_bench:
+            for i in _range(total):
+                a += i
+                our_file.write(a)
 
     # Compute relative overhead of tqdm against native range()
     try:
-        assert(time_tqdm() < 9 * time_bench())
+        assert(time_tqdm() < 3 * time_bench())
     except AssertionError:
         raise AssertionError('trange(%g): %f, range(%g): %f' %
                              (total, time_tqdm(), total, time_bench()))
@@ -69,7 +76,7 @@ def test_manual_overhead():
     """ Test overhead of manual tqdm """
     total = int(1e6)
 
-    with closing(StringIO()) as our_file:
+    with closing(MockFileNoWrite()) as our_file:
         t = tqdm(total=total * 10, file=our_file, leave=True)
         a = 0
         with relative_timer() as time_tqdm:
@@ -77,14 +84,68 @@ def test_manual_overhead():
                 a += i
                 t.update(10)
 
-    a = 0
-    with relative_timer() as time_bench:
-        for i in _range(total):
-            a += i
+        a = 0
+        with relative_timer() as time_bench:
+            for i in _range(total):
+                a += i
+                our_file.write(a)
 
     # Compute relative overhead of tqdm against native range()
     try:
-        assert(time_tqdm() < 19 * time_bench())
+        assert(time_tqdm() < 10 * time_bench())
+    except AssertionError:
+        raise AssertionError('tqdm(%g): %f, range(%g): %f' %
+                             (total, time_tqdm(), total, time_bench()))
+
+
+def test_iter_overhead_hard():
+    """ Test overhead of iteration based tqdm (hard) """
+    total = int(1e5)
+
+    with closing(MockFileNoWrite()) as our_file:
+        a = 0
+        with relative_timer() as time_tqdm:
+            for i in trange(total, file=our_file, leave=True,
+                                miniters=1, mininterval=0, maxinterval=0):
+                a += i
+        assert(a == (total * total - total) / 2.0)
+
+        a = 0
+        with relative_timer() as time_bench:
+            for i in _range(total):
+                a += i
+                our_file.write(("%i" % a)*40)
+
+    # Compute relative overhead of tqdm against native range()
+    try:
+        assert(time_tqdm() < 30 * time_bench())
+    except AssertionError:
+        raise AssertionError('trange(%g): %f, range(%g): %f' %
+                             (total, time_tqdm(), total, time_bench()))
+
+
+def test_manual_overhead_hard():
+    """ Test overhead of manual tqdm (hard) """
+    total = int(1e5)
+
+    with closing(MockFileNoWrite()) as our_file:
+        t = tqdm(total=total * 10, file=our_file, leave=True,
+                    miniters=1, mininterval=0, maxinterval=0)
+        a = 0
+        with relative_timer() as time_tqdm:
+            for i in _range(total):
+                a += i
+                t.update(10)
+
+        a = 0
+        with relative_timer() as time_bench:
+            for i in _range(total):
+                a += i
+                our_file.write(("%i" % a)*40)
+
+    # Compute relative overhead of tqdm against native range()
+    try:
+        assert(time_tqdm() < 50 * time_bench())
     except AssertionError:
         raise AssertionError('tqdm(%g): %f, range(%g): %f' %
                              (total, time_tqdm(), total, time_bench()))
